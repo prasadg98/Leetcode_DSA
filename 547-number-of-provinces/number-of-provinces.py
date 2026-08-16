e0cebbd1486bb8b11de34edaa7a1ec23d8f144e0
Prasad Gavas <145227919+prasadg98@users.noreply.github.com>
2026-08-16 16:48:07 -0700

    Time: 4 ms (66.18%) | Memory: 20.3 MB (89.07%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,19 +1,20 @@
 class Solution:
-    def dfs(self, i, adjMatrix, visited):
-        visited[i] = True
-        for x in range(len(adjMatrix)):
-            if adjMatrix[i][x] == 1 and not visited[x]:
-                self.dfs(x,adjMatrix, visited)
 
+    def dfs(self, i, isConnected, visited):
+        visited[i] = True
+        for x in range(len(isConnected[i])):
+            if isConnected[i][x] == 1 and visited[x] == False:
+                self.dfs(x, isConnected, visited)
 
 
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        visited = [False] *  n
-        ans = 0
+        visited = [False] * n
+        total_provinces = 0
+
         for i in range(n):
             if not visited[i]:
                 self.dfs(i, isConnected, visited)
-                ans += 1
+                total_provinces += 1
 
-        return ans
+        return total_provinces
